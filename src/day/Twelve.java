3b1cf8b7e6a5598f66b419ff1ef306de7338a5a3
Gingmin <kkmk220@gmail.com>
2020-12-29 01:59:56 +0900

    12.28
    12.28

---
+++ src/day/Twelve.java
@@ -0,0 +1,26 @@
+package day;
+
+public class Twelve {
+
+	public static void main(String[] args) {
+
+/* 		12.28
+ * 
+		# 1. Subquery
+
+		- 쿼리 문 안에서 사용된 쿼리문
+		- 서브쿼리는 SELECT, FROM, WHERE, HAVING, ORDER BY 절에도 사용할 수 있다.
+		- 다중행 서브쿼리(서브쿼리 결과가 한 개의 행?)
+		- 다중열 서브쿼리 (서브쿼리가 여러 개?)
+
+		> FROM절에서 서브쿼리를 사용할 수 있다 이를 인라인 뷰(INLINE VIEW)라고함
+
+		- ROWNUM은 행 번호
+		- ORDER BY 한 다음에 ROWNUM이 붙게 하려면 서브쿼리(인라인뷰)를 사용해야 함
+		- RANK() : 동일한 순위 이후의 등수를 동일한 인원만큼 건너 뛰고 다음 순위를 계산하는 방식
+		DENSE_RANK() : 중복되는 순위 이후의 등수를 등수로 처리
+		- 스칼라 서브쿼리 : 단일행 서브쿼리이면서, 상관쿼리를 사용
+*/
+	}
+
+}
